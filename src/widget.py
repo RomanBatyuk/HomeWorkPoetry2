66dbe13ce290800d9203b2f31b892581e5c57331
Roman <neznayuhto@gmail.com>
2025-03-01 23:26:38 +0300

    проверка кода на правильность написания

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from src.masks import get_mask_card_number, get_mask_account
+from masks import get_mask_card_number, get_mask_account
 from datetime import datetime
 
 
@@ -20,18 +20,8 @@ def mask_account_card(card_or_account: str) -> str:
     str_numbers_card = "".join(numbers_card)
     return f"{name_card[0]} {name_card[1]} {get_mask_card_number(str_numbers_card)}"
 
+
 def get_date(my_date: str) -> str:
     """Функция конвертирования даты"""
     date_obj = datetime.strptime(my_date, "%Y-%m-%dT%H:%M:%S.%f")
     return date_obj.strftime("%d.%m.%Y")
-
-
-print(mask_account_card("Visa Platinum 1234567891234567"))
-print(get_date("2024-03-11T02:26:18.671407"))
-
-print(mask_account_card("Visa Platinum 1234567891234567"))
-print(mask_account_card("Maestro 1596837868705199"))
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 7158300734726758"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(get_date("2024-03-11T02:26:18.671407"))
